01ee5cd558a54d1e827efb61802646f35b5a0337
Mark Pham <pham.manh.cuong.sg@gmail.com>
2023-04-21 17:24:46 +0700

    Set Solution architecture

---
+++ eVoucher.ClientAPI_Integration/BaseAPIClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eVoucher.ClientAPI_Integration
+{
+    public interface IBaseAPIClient
+    {
+
+    }
+    public class BaseAPIClient:IBaseAPIClient
+    {
+        protected const string BASE_URL = "https://localhost:7233/api/";
+        protected HttpClient _httpClient;
+        public BaseAPIClient()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(BASE_URL);
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
